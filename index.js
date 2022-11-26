9b6a997e0594533605dd9e5fdbed46d23acf1375
Mink <109709736+PTTMinh@users.noreply.github.com>
2022-11-26 22:34:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,72 +1,262 @@
-const obj = {
-    mID: document.querySelector("#my-id"),
-    fID: document.querySelector("#f-id"),
-    conBtn: document.querySelector("#conn"),
-    msg: document.querySelector("#msg"),
-    send: document.querySelector("#send"),
-    Cdisplay: document.querySelector("main"),
-};
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
 
-var conn = new Peer();
-var Aconn = undefined;
+    <div class="nav-bar">
+        <ul>
+            <li><a href="#"><i class="fa fa-tachometer"></i>DASHBOARD</a></li>
+            <li><a href="#"><i class="fa fa-male"></i>WORKER</a></li>
+            <li><a href="#"><i class="fa fa-truck"></i>VEHICLE</a></li>
+            <li><a href="#"><i class="fa fa-trash"></i>MCP</a></li>
+            <li><a href="#"><i class="fa fa-phone"></i>CONTACT</a></li>
+            <li><a href="#"><i class="fa fa-sign-out"></i>LOGOUT</a></li>
+        </ul>
+    </div>
 
-conn.on("open", (id) =>{
-    obj.mID.value = id;
-});
+    <div class = "container">
+        <div class = "leftSide">
+            <!-- header -->
+            <div class="header">
+                <img src="image/Back officer.png" class="ava">
+                <div class="c1"> 
+                    <label><strong>Back Officer</strong></label>
+                    <br>
+                    <input type="text" id="my-id" class="myID">
+                </div>
+            </div>
 
-conn.on("connection", (NAConn) => {
-    if(Aconn != undefined) Aconn.close();
-    obj.fID.value = NAConn.peer;
-    Aconn = NAConn;
-    Aconn.on("data", displayFriendMSG);
-})
+            <!-- search -->
+            <div class="search_chat">
+                <div>
+                    <input type="text" placeholder="Input worker's ID" id="f-id" class="fID">
+                    <ion-icon name="search-outline"></ion-icon>
+                    <button id="conn" class="connect">Connect</button>
+                </div>
+            </div>
+            
+            <!-- chat list -->
+            <div class="chatlist">
+                <div class="block unread">
+                    <div class="imgbx">
+                        <img src="/image/Back officer.png" class="ava">
+                    </div>
+                    <div class="details">
+                        <div class="listHead">
+                            <h4>Mẫn Cao Kỳ</h4>
+                            <p class="time">10:56</p>
+                        </div>
+                        <div class="message_p">
+                            <p>Task has been assigned</p>
+                            <b>1</b>
+                        </div>
+                    </div>
+                </div>
 
-obj.conBtn.addEventListener("click", () => {
-    if(obj.fID.value.length > 0) {
-        if(Aconn != undefined) Aconn.close();
-        Aconn = conn.connect(obj.fID.value);
-        Aconn.on("data", displayFriendMSG);
-    }
-});
+                <div class="block active">
+                    <div class="imgbx">
+                        <img src="image/Back officer.png" class="ava">
+                    </div>
+                    <div class="details">
+                        <div class="listHead">
+                            <h4>Điêu Duy Quang</h4>
+                            <p class="time">10:56</p>
+                        </div>
+                        <div class="message_p">
+                            <p>Task has been assigned</p>
+                        </div>
+                    </div>
+                </div>
 
-obj.send.addEventListener("click", () => {
-    if(Aconn != undefined){
-        if(obj.msg.value.length > 0) {
-            sendData(obj.msg.value);
-            displayMyMSG();
-            obj.msg.value = "";
-        }
-    }
-})
+                <div class="block">
+                    <div class="imgbx">
+                        <img src="image/Back officer.png" class="ava">
+                    </div>
+                    <div class="details">
+                        <div class="listHead">
+                            <h4>Tiếp Hữu Vượng</h4>
+                            <p class="time">10:56</p>
+                        </div>
+                        <div class="message_p">
+                            <p>Task has been assigned</p>
+                        </div>
+                    </div>
+                </div>
 
-function displayMyMSG(){
-    obj.Cdisplay.insertAdjacentHTML("beforeend", `
-    <div class="my-msg">
-        <div>
-            <div class="title">Me</div>
-            <div class="content-msg">
-                ${obj.msg.value}
+                <div class="block">
+                    <div class="imgbx">
+                        <img src="image/Back officer.png" class="ava">
+                    </div>
+                    <div class="details">
+                        <div class="listHead">
+                            <h4>Ngạc Việt Phương</h4>
+                            <p class="time">10:56</p>
+                        </div>
+                        <div class="message_p">
+                            <p>Task has been assigned</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="block">
+                    <div class="imgbx">
+                        <img src="image/Back officer.png" class="ava">
+                    </div>
+                    <div class="details">
+                        <div class="listHead">
+                            <h4>Điêu Duy Quang</h4>
+                            <p class="time">10:56</p>
+                        </div>
+                        <div class="message_p">
+                            <p>Task has been assigned</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="block">
+                    <div class="imgbx">
+                        <img src="image/Back officer.png" class="ava">
+                    </div>
+                    <div class="details">
+                        <div class="listHead">
+                            <h4>Thi Trọng Khánh</h4>
+                            <p class="time">10:56</p>
+                        </div>
+                        <div class="message_p">
+                            <p>Task has been assigned</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="block">
+                    <div class="imgbx">
+                        <img src="image/Back officer.png" class="ava">
+                    </div>
+                    <div class="details">
+                        <div class="listHead">
+                            <h4>Đỗ Thiện Minh</h4>
+                            <p class="time">10:56</p>
+                        </div>
+                        <div class="message_p">
+                            <p>Task has been assigned</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="block">
+                    <div class="imgbx">
+                        <img src="image/Back officer.png" class="ava">
+                    </div>
+                    <div class="details">
+                        <div class="listHead">
+                            <h4>Liễu Thiên Mạnh</h4>
+                            <p class="time">10:56</p>
+                        </div>
+                        <div class="message_p">
+                            <p>Task has been assigned</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="block">
+                    <div class="imgbx">
+                        <img src="image/Back officer.png" class="ava">
+                    </div>
+                    <div class="details">
+                        <div class="listHead">
+                            <h4>Phùng Thiện Ngôn</h4>
+                            <p class="time">10:56</p>
+                        </div>
+                        <div class="message_p">
+                            <p>Task has been assigned</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="block">
+                    <div class="imgbx">
+                        <img src="image/Back officer.png" class="ava">
+                    </div>
+                    <div class="details">
+                        <div class="listHead">
+                            <h4>Kông Ngọc Khôi</h4>
+                            <p class="time">10:56</p>
+                        </div>
+                        <div class="message_p">
+                            <p>Task has been assigned</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="block">
+                    <div class="imgbx">
+                        <img src="image/Back officer.png" class="ava">
+                    </div>
+                    <div class="details">
+                        <div class="listHead">
+                            <h4>Vưu Nghĩa Dũng</h4>
+                            <p class="time">10:56</p>
+                        </div>
+                        <div class="message_p">
+                            <p>Task has been assigned</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="block">
+                    <div class="imgbx">
+                        <img src="image/Back officer.png" class="ava">
+                    </div>
+                    <div class="details">
+                        <div class="listHead">
+                            <h4>Quách Hữu Hiệp</h4>
+                            <p class="time">10:56</p>
+                        </div>
+                        <div class="message_p">
+                            <p>Task has been assigned</p>
+                        </div>
+                    </div>
+                </div>                
+
             </div>
         </div>
-    </div>
-    `)
-}
 
-function displayFriendMSG(data){
-    obj.Cdisplay.insertAdjacentHTML("beforeend", `
-    <div class="friend-msg">
-        <div>
-            <div class="title">${obj.fID.value}</div>
-            <div class="content-msg">
-                ${data}
+        <div class = "rightSide">
+            <div class="header">
+                <div class="imgText">
+                    <img src="image/Back officer.png" class="ava">
+                    <h4>Back Officer<br><span>Online</span></h4>
+                </div>
+            </div>
+
+            <!-- Chat box -->
+            <div class="chatBox">
+                <div class="message my_message">
+                    <div class="content-my-message"></div>
+                </div>
+                <div class="message friend_message">
+                    <div class="content-friend-message"></div>
+                </div>
+
+            </div>
+
+            <!-- Chat input -->
+            <div class="chatbox_input">
+                <input type="text" placeholder="Type a message" id="message">
+                <ion-icon name="send-outline" id="send"></ion-icon>
             </div>
         </div>
-    </div>
-    `)
-}
+    </div>    
 
-function sendData(data){
-    if(Aconn != undefined){
-        Aconn.send(data);
-    }
-}
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <script src="https://unpkg.com/peerjs@1.4.7/dist/peerjs.min.js"></script>
+    <script defer src="send.js"></script>
+</body>
+</html>
